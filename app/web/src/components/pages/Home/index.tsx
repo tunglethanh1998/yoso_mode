d6feeffc877807b3bc2ad64f39dbdf7990fcaab9
tunglethanh1998 <tung_le_thanh@bita.jp>
2025-12-12 15:07:40 +0700

    feat: init 8

--- app/web/src/components/pages/Home/index.tsx
+++ app/web/src/components/pages/Home/index.tsx
@@ -1,7 +1,54 @@
+"use client";
+import { ROUTE } from "@/libs/enum";
+import { Button } from "@mui/material";
+import Link from "next/link";
+
 export const HomePage = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1>HomePage</h1>
+    <div className="flex flex-col items-center justify-center">
+      <img
+        src="/static/1.webp"
+        alt="Home"
+        className="w-full h-full object-cover"
+      />
+      <img
+        src="/static/2.webp"
+        alt="2"
+        className="w-full h-full object-cover"
+      />
+      <img
+        src="/static/3.webp"
+        alt="3"
+        className="w-full h-full object-cover"
+      />
+      <img
+        src="/static/4.webp"
+        alt="4"
+        className="w-full h-full object-cover"
+      />
+      <img
+        src="/static/5.webp"
+        alt="5"
+        className="w-full h-full object-cover"
+      />
+      <img
+        src="/static/6.webp"
+        alt="6"
+        className="w-full h-full object-cover"
+      />
+      <img
+        src="/static/7.webp"
+        alt="7"
+        className="w-full h-full object-cover"
+      />
+
+      <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ">
+        <Link href={`${ROUTE.NEWS}?type=column&page=1`}>
+          <Button variant="contained" color="primary">
+            News Column
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 };
--- app/web/src/libs/enum.ts
+++ app/web/src/libs/enum.ts
@@ -2,7 +2,6 @@ export enum ROUTE {
   HOME = "/",
   LOGIN = "/login",
   NEWS = "/news",
-  NEWS_DETAILS = "/news/[type]/[id]",
 
   NOT_FOUND = "/not-found",
 }

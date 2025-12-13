6d3290e053207e301b2795690339a27533e08bc5
tunglethanh1998 <tung_le_thanh@bita.jp>
2025-12-13 08:03:27 +0700

    feat: init 15

--- app/cms/src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20251211_031001 from './20251211_031001';
-
-export const migrations = [
-  {
-    up: migration_20251211_031001.up,
-    down: migration_20251211_031001.down,
-    name: '20251211_031001'
-  },
-];

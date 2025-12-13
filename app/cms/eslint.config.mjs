cd651d64efc83c33e934ddd40f9eb119836cdbf7
tunglethanh1998 <tung_le_thanh@bita.jp>
2025-12-13 09:50:10 +0700

    feat: init 23

--- app/cms/eslint.config.mjs
+++ app/cms/eslint.config.mjs
@@ -16,6 +16,7 @@ const eslintConfig = [
       '@typescript-eslint/ban-ts-comment': 'warn',
       '@typescript-eslint/no-empty-object-type': 'warn',
       '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': [
         'warn',
         {
--- app/cms/src/payload.config.ts
+++ app/cms/src/payload.config.ts
@@ -2,14 +2,30 @@ import { postgresAdapter } from '@payloadcms/db-postgres'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import path from 'path'
 import { buildConfig } from 'payload'
-import { fileURLToPath } from 'url'
 import sharp from 'sharp'
+import { fileURLToPath } from 'url'
 
-import { Users } from './collections/Users'
+import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
+import { s3Storage } from '@payloadcms/storage-s3'
 import { Media } from './collections/Media'
+import { News } from './collections/News'
+import { Users } from './collections/Users'
+import { S3_PREFIX } from './libs/enums'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
+
+const parseEnvList = (envValue?: string): string[] => {
+  if (!envValue?.trim()) {
+    return []
+  }
+  return envValue
+    ? envValue
+        .split(',')
+        .map((origin) => origin.trim())
+        .filter(Boolean)
+    : []
+}
 
 export default buildConfig({
   admin: {
@@ -18,7 +34,7 @@ export default buildConfig({
       baseDir: path.resolve(dirname),
     },
   },
-  collections: [Users, Media],
+  collections: [Users, Media, News],
   editor: lexicalEditor(),
   secret: process.env.PAYLOAD_SECRET || '',
   typescript: {
@@ -29,6 +45,28 @@ export default buildConfig({
       connectionString: process.env.DATABASE_URI || '',
     },
   }),
+  cors: parseEnvList(process.env.CORS_ORIGINS),
+  csrf: parseEnvList(process.env.CSRF_ORIGINS),
   sharp,
-  plugins: [],
+  plugins: [
+    payloadCloudPlugin(),
+    s3Storage({
+      bucket: process.env.AWS_S3_BUCKET_NAME || '',
+      config: {
+        region: process.env.AWS_S3_BUCKET_REGION,
+        credentials: {
+          accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID || '',
+          secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY || '',
+        },
+      },
+      collections: {
+        media: {
+          prefix: S3_PREFIX.PUBLIC_ASSETS,
+          generateFileURL: async ({ filename }) => {
+            return `${process.env.AWS_CLOUDFRONT_PREFIX}/${S3_PREFIX.PUBLIC_ASSETS}/${encodeURIComponent(filename)}`
+          },
+        },
+      },
+    }),
+  ],
 })
